d380746a5a0a9cd400989b85abb0c63343078a15
ryo-makiyama <ryo.makiyama@excite.jp>
2017-10-16 20:53:42 +0900

    add left sample

--- app/src/main/java/rmakiyama/sample/mvvmsample/left/LeftViewModel.java
+++ app/src/main/java/rmakiyama/sample/mvvmsample/left/LeftViewModel.java
@@ -1,6 +1,8 @@
 package rmakiyama.sample.mvvmsample.left;
 
 import android.databinding.ObservableField;
+import android.databinding.ObservableInt;
+import android.view.View;
 
 import rmakiyama.sample.mvvmsample.ViewModel;
 
@@ -11,7 +13,17 @@ import rmakiyama.sample.mvvmsample.ViewModel;
 public class LeftViewModel extends ViewModel {
 
     public final ObservableField<String> description = new ObservableField<>("Leftのフラグメントです");
+    public final ObservableInt number = new ObservableInt(0);
+    public final ObservableField<String> input = new ObservableField<>();
 
     public LeftViewModel() {
+    }
+
+    public void onClickAdd(@SuppressWarnings("unused") View view) {
+        number.set(number.get() + 1);
+    }
+
+    public void onClickClear(@SuppressWarnings("unused") View view) {
+        input.set("");
     }
 }
